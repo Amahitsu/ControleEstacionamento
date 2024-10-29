36c53a3b47e0c8ce2d0d1b0b78ca855c1b51c2e6
Amanda H Medeiros <71605324+Amahitsu@users.noreply.github.com>
2024-10-29 20:30:24 -0300

    alterações da router para DELETE no cupom

--- src/app/api/cupom/route.tsx
+++ src/app/api/cupom/route.tsx
@@ -1,5 +1,6 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function GET(): Promise<NextResponse> {
   try {
@@ -26,12 +27,24 @@ export async function PUT(
     }
 }
 
-export async function DELETE(id: number): Promise<NextResponse> {
+export async function DELETE(request: Request): Promise<NextResponse> {
   try {
-    const { rows } = await sql`DELETE FROM "cupom"
-      WHERE id = ${id}`;
-      return NextResponse.json({ data: rows }, { status: 200 });
-    } catch (error) {
-      return NextResponse.json({ data: error }, { status: 500 });
+    const { id } = await request.json(); // Lendo o ID do corpo da requisição
+
+    // Executando o DELETE
+    const result = await sql`
+      DELETE FROM "cupom"
+      WHERE id = ${id}
+    `;
+
+    // Se result não tem rowCount, pode simplesmente verificar se houve erro
+    if (result?.rowCount && result.rowCount > 0) {
+      return NextResponse.json({ message: 'Cupom deletado com sucesso!' }, { status: 200 });
+    } else {
+      return NextResponse.json({ message: 'Cupom não encontrado' }, { status: 404 });
     }
+  } catch (error: unknown) {
+    const errorMessage = error instanceof Error ? error.message : 'Erro desconhecido';
+    return NextResponse.json({ message: 'Erro ao deletar o cupom', error: errorMessage }, { status: 500 });
+  }
 }
