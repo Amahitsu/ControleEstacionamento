403312544e337316faadf623621296f22ce714bb
Amanda H Medeiros <71605324+Amahitsu@users.noreply.github.com>
2024-11-01 20:04:13 -0300

    Adicionando configuração para API para produção e local

---
+++ src/app/config/config.ts
@@ -0,0 +1,13 @@
+// src/config/config.ts
+
+const baseApiUrl = process.env.NODE_ENV === 'production'
+  ? 'https://controle-estacionamento-lovat.vercel.app/api'
+  : 'http://localhost:3000/api';
+
+export const apiUrls = {
+  cupons: `${baseApiUrl}/cupom`,
+  modelos: `${baseApiUrl}/modelo`,
+  tipoVeiculo: `${baseApiUrl}/tipoVeiculo`,
+
+  // Adicione outros endpoints conforme necessário
+};
