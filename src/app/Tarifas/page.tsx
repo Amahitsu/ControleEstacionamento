b045bc82bd8ae6f3d7f08d9f38e6b18ed4aef27a
Amanda H Medeiros <71605324+Amahitsu@users.noreply.github.com>
2024-11-04 20:00:22 -0300

    Adicionando pasta para segunda tela do projeto

---
+++ src/app/Tarifas/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CadastrarTarifas from '../components/CadastrarTarifa'; // Ajuste o caminho conforme necessário
+
+const TarifaCadastrada: React.FC = () => {
+  return (
+    <div>
+      <h1>Segunda Tela - Cadastrar Tarifa</h1>
+      <CadastrarTarifas />
+    </div>
+  );
+};
+
+export default TarifaCadastrada;
